1ad88676f37550b7d25b6bd9f278346b1853cc11
unknown <devin@zovkov.com>
2016-04-21 11:14:21 -0700

    added some color

---
+++ public_html/js/Todo.js
@@ -0,0 +1,39 @@
+$(function () {
+    var APPLICATION_ID = "8572240B-0E4B-C9FB-FF24-655FDD401400",
+        SECRET_KEY = "D0CF89B9-4986-260D-FF0A-DBA66B417900",
+        VERSION = "v1";
+        
+    Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+     
+    
+     
+  
+     var postsCollection = Backendless.Persistence.of(Posts).find();
+   
+     console.log(postsCollection);
+     
+     var wrapper = {
+         posts: postsCollection.data
+     };
+     
+     
+     
+     Handlebars.registerHelper('format', function (time) {
+         return moment(time).format("dddd, MMMM Do YYYY");
+     });
+     
+     var blogScript = $("#blogs-template").html();
+     var blogTemplate = Handlebars.compile(blogScript);
+     var blogHTML = blogTemplate(wrapper);
+     
+     $('.main-container').html(blogHTML);
+});
+
+
+
+function Posts(args) {
+    args = args || {};
+    this.title = args.title || "";
+    this.content = args.content || "";
+    this.authorEmail = args.authorEmail || "";
+}
